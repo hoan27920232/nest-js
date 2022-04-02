287eddc791cf89ffa71dff1891d503ce7c6ad2f8
unknown <allfallsdown20@gmail.com>
2022-04-02 14:50:40 +0700

    CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IdeaModule } from './idea/idea.module';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), IdeaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/idea/idea.dto.ts
@@ -0,0 +1,6 @@
+/* eslint-disable */
+
+export interface IdeaDTO {
+    idea: string;
+    description: string;
+}
